1be0d9d6ea9313a20cf1378bd9757a8a6262b5db
Pinae <jonny@pinae.net>
2018-01-15 14:42:30 +0100

    Basic script for creating CSRs with AltNames

---
+++ create-csr.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+echo "Length of the RSA-Key? [4096]"
+read key_size
+if [ "${key_size:=4096}" -lt 2048 ]; then
+    key_size=4096
+    echo "Key size too small. Setting to 4096."
+fi
+days=93
+echo "Country? [DE]"
+read Country
+echo "State? [Berlin]"
+read State
+echo "City? [Berlin]"
+read City
+echo "Organization name? [${1}]"
+read Organization
+if [ "${Organization:-0}" ]; then
+    Organization=${1}
+fi
+echo "Organization unit? [${1}-Webmaster]"
+read Organization_Unit
+if [ "${Organization:-0}" ]; then
+    Organization_Unit=${1}"-Webmaster"
+fi
+echo "Webmaster Email? [webmaster@${1}]"
+read Email
+if [ "${Email:-0}" ]; then
+    Email="webmaster@"${1}
+fi
+DNSNames=""
+for ((i=1;i <= $#;i++))
+{
+    eval domain_name=\$$i
+    DNSNames=${DNSNames}"DNS."${i}" = "${domain_name}$'\n'
+}
+
+openssl req -new -sha256 -nodes -out ${1}.csr -newkey rsa:${key_size} -days ${days} -keyout ${1}.key -config <(
+cat <<-EOF
+[req]
+default_bits = ${key_size}
+prompt = no
+default_md = sha256
+req_extensions = req_ext
+distinguished_name = dn
+
+[ dn ]
+C=${Country:="DE"}
+ST=${State:="Berlin"}
+L=${City:="Berlin"}
+O=${Organization}
+OU=${Organization_Unit:=""}
+emailAddress=${Email}
+CN = ${1}
+
+[ req_ext ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+${DNSNames}
+EOF
+)
